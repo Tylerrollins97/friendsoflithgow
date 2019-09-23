0db9b43158f4a7f3dfdc5b3a4b9dcd77deeb2651
Tyler Rollins <46821777+Tylerrollins97@users.noreply.github.com>
2019-09-23 13:42:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -40,3 +40,19 @@ const navSlide = () => {
 
   navSlide();
 //!NAV//
+
+//NAV ON SCROLL//
+
+$(window).scroll({
+    previousTop: 0
+},
+
+function () {
+    var currentTop = $(window).scrollTop();
+    if (currentTop < this.previousTop) {
+        $("header").slideUp();
+    } else {
+        $("header").slideDown();
+    }
+    this.previousTop = currentTop;
+});
