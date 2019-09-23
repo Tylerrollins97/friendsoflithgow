569faa662265cd3475cf84073772e846085e9999
Tyler Rollins <46821777+Tylerrollins97@users.noreply.github.com>
2019-09-23 10:59:06 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+
+//NAV//
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+      //Toggle Nav
+      nav.classList.toggle('nav-active');
+
+      //Animate links
+      navLinks.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation =''
+        } else{
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`
+        }
+      });
+      //Burger Animation
+      burger.classList.toggle('toggle');
+  });
+
+  nav.addEventListener('click', () => {
+      //Toggle Nav
+      nav.classList.toggle('nav-active');
+
+      //Animate links
+      navLinks.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation =''
+        } else{
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`
+        }
+      });
+      //Burger Animation
+      burger.classList.toggle('toggle');
+  });
+}
+
+  navSlide();
+//!NAV//
